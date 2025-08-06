29cacedd12a1ddf81ceaf04a5ba678bf5fe3acdf
yağmur varol <yagmurvarol@yagmurs-MacBook-Pro.local>
2025-08-06 10:12:50 +0300

    class file

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Cell:
+
+    def __init__(self, value, row, column, size, editable = True):
+        self.value = value
+        self.row = row
+        self.col = column
+        self.size = size
+        self.edit = editable
+        self.selected = Falsecd ..
+
+    def draw(self, surface, font):
+        """
+        Draw cells of the sudoku board.
+        """
+        x = self.col * self.size
+        y = self.row * self.size
+
+
